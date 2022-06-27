334f34b73d3d33b66a161952028c5dc49130567a
Paulo Victor <pvictor36@gmail.com>
2022-06-27 15:07:45 -0300

    Criando projeto ReactJs e criando componentes Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXjhqjeMVjxawyUvuL4gzRRAgj85iYJwJOVQ&usqp=CAU" alt="imagem de teste" className="img"/>
+        </>
+    );
+}
+
+export default Home;
