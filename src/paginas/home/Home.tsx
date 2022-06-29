4cbc9bc24f9bb5e226bb214e66edfaf1bdc80b51
Paulo Victor <pvictor36@gmail.com>
2022-06-29 17:24:41 -0300

    Ajustanto configurações e importações do Material-UI, e brincando com Grid e flex-box

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,23 +1,24 @@
-import React from "react";
-import { Grid, Paper } from "@material-ui/core";
+
+import React from 'react';
+import { Button, Paper } from '@material-ui/core';
 import './Home.css';
+import { Box } from '@mui/material';
 
 function Home() {
     return (
         <>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={8}>
-                    <Paper style={{ height: "100vh", background: "blue"}} />
-                </Grid>
-                <Grid item container direction="column" xs={12} sm={4} spacing={2}>
-                    <Grid item>
-                        <Paper style={{ height: "49vh", background: "orange"}} />
-                    </Grid>
-                    <Grid item>
-                        <Paper style={{ height: "49vh", background: "green"}} />
-                    </Grid>
-                </Grid>
-            </Grid>
+           <Paper>
+                <Box p={2}>
+                    <Box display="flex" justifyContent="center">
+                        <h1>Título</h1>
+                    </Box>
+                    <img src="https://img.olhardigital.com.br/wp-content/uploads/2021/11/One-Piece-Sem-Borda.jpg" alt="Imagem de One Piececap. 1000" style={{ width: "100%" , height: "100%" }} />
+                    <Box display="flex" justifyContent="center" p={3}>
+                        <Button variant='contained' color='primary'>Botão 1</Button>
+                        <Button variant='contained' color='secondary'>Botão 2</Button>
+                    </Box>
+                </Box>
+           </Paper>
         </>
     );
 }
