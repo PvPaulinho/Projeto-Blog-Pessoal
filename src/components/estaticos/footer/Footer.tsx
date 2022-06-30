099c7bceea6322f40113a6fd8b4c7ca491be5c86
Paulo Victor <pvictor36@gmail.com>
2022-06-30 02:12:11 -0300

    Criando sistema de rotas e ajustando estilização

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -2,35 +2,36 @@ import { Grid, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
 import { GitHub, Instagram, LinkedIn } from "@material-ui/icons";
 import React from "react";
+import "./Footer.css";
 
 function Footer (){
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className="box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className="textos">Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/PvPaulinho" target="_blank">
-                                <GitHub style={{ fontSize: 60, color: "white" }} />
+                                <GitHub className="redes" />
                             </a>
                             <a href="https://www.instagram.com/paulo_victor_damasceno_silva/" target="_blank">
-                                <Instagram style={{ fontSize: 60, color: "white" }} />
+                                <Instagram className="redes" />
                             </a>
                             <a href="https://www.linkedin.com/in/paulo-victor-damasceno-e-silva-0184ba183/" target="_blank">
-                                <LinkedIn style={{ fontSize: 60, color: "white" }} />
+                                <LinkedIn className="redes" />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className="box2">
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2022 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className="textos" >© 2022 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://blog-pessoal-paulovictor.herokuapp.com">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Paulo Victor Damasceno e Silva</Typography>
+                                <Typography variant="subtitle2" gutterBottom className="textos" align="center">Paulo Victor Damasceno e Silva</Typography>
                             </a>
                         </Box>
                     </Box>
